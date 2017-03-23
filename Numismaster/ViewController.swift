98d46846eedd0d9fd8d8b44c17316ba75ee13b10
Andres Gonzalez Casabianca <gonche1124@gmail.com>
2017-03-22 20:50:59 -0400

    Initial Commit

---
+++ Numismaster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Numismaster
+//
+//  Created by Andres Gonzalez Casabianca on 3/22/17.
+//  Copyright © 2017 Gonche. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
